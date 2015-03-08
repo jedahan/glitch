c2205924d67339fbec31753cf439efdcbfec5dcc
Jonathan Dahan <jonathan@jedahan.com>
2015-03-08 14:03:23 -0400

    b

--- video.jpg.sh
+++ video.jpg.sh
@@ -1,1 +1,1 @@
-convert huehue.jpg rgb:- | ffmpeg -f rawvideo -r 7.5 -s 160x120 -pix_fmt monow -f rawvideo -i - -pix_fmt yuv420p -c:v rawvideo -f matroska - | mpv -
+convert video.jpg rgb:- | ffmpeg -f rawvideo -r 7.5 -s 160x120 -pix_fmt monow -f rawvideo -i - -pix_fmt yuv420p -c:v rawvideo -f matroska - | mpv -
